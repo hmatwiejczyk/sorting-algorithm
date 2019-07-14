0ec6a20e2fc5f1c9195b3eb154db38874b2fe430
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-14 15:13:44 +0200

    add ISorter interface

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,11 +1,15 @@
-import { NumbersCollection } from './NumbersCollection';
-
+interface ISorter {
+  length: number;
+  compare(leftIndex: number, rightIndex: number) : boolean;
+  swap(leftIndex: number, rightIndex: number) : void;
+}
 export class Sorter {
-  constructor(public collection: NumbersCollection) {}
+  constructor(public collection: ISorter) {}
 
   sort(): void {
-    for (let i = 0; i < this.collection.length; i++) {
-      for (let j = 0; j < this.collection.length - i - 1; j++) {
+    let { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
         if (this.collection.compare(j, j + 1)) {
           this.collection.swap(j, j + 1);
         }
--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ const numbersCollection = new NumbersCollection([1, 0, -22, 5, -2])
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 
-console.log(sorter.collection);
+console.log(numbersCollection.data);
